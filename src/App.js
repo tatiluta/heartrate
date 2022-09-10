9cc0b8ac80978d47c00a0ea922ba51633cb3c225
Tatiana Luta <tati@Tatianas-MacBook-Pro.local>
2022-09-10 21:23:09 +0300

    heartrate calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [result, setResult] = useState('')
+  const [age, setAge] = useState(0)
+
+
+  function calculate() {
+    const upper = (220 - age) * 0.85;
+    const lower = (220 - age) * 0.65;
+    setResult(lower + "-" + upper);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="content">
+      <h2>Heart rate limits calculator </h2>
+      <form>
+        <div>
+          <label>Age</label><p></p>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)} />
+        </div>
+        <div>
+          <label>Heart rate limits</label><p></p>
+        </div>
+        <div id="box">
+          <output>{result}</output>
+        </div>
+        <button type="button" class="btn btn-outline-dark" onClick={calculate}>Calculate</button>
+      </form>
     </div>
   );
 }
-
 export default App;
